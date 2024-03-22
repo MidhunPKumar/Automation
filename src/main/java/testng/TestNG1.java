816a7db9131b2997fa01565ceb90b8e44052038c
Midhun <midhunpk2015@gmail.com>
2024-03-22 17:52:44 +0530

    third

--- src/main/java/testng/TestNG1.java
+++ src/main/java/testng/TestNG1.java
@@ -3,7 +3,6 @@ package testng;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.*;
 
 public class TestNG1 {
 	
